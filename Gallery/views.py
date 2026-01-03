3e2716d4a52b43d6569b7d00561629ad0376ad1d
sholved <dianemaggie@gmail.com>
2026-01-03 23:44:36 +0000

    commit for today

--- Gallery/views.py
+++ Gallery/views.py
@@ -5,6 +5,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
+from .models import Image
+from .upload import upload_image
 
 User = get_user_model()
 
@@ -20,8 +22,31 @@ class ProtectedUser(APIView):
     def get(self, request):
         return Response(f'Hello, {request.user.username}, You are Authenticated')
 
+
+class UploadImageView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     
-    
+    def post(self, request):
+        file = request.FILES.get("image")
+        title = request.data.get("title")
+        
+        if not file:
+            return Response(f'{"error":  "No file provided", status=400}')
+        
+        image = Image.objects.create(
+            owner=request.user,
+            title = title
+        )
+        
+        path = upload_image(file, request.user, image.id)
+        image.storage_path = path
+        image.save()
+        
+        return Response(f'{
+            "id": str(image.id),
+            "path": image.storage_path}, status= 201')
+        
+        
 # class ReminderList(generics.ListCreateAPIView):
 #     serializer_class = ReminderSerializer
 #     permission_classes = [permissions.IsAuthenticated]
