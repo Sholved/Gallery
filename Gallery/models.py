5895780a3a6c08c0eb1cafe5c125b3e0e690fdc8
sholved <dianemaggie@gmail.com>
2026-01-09 22:05:46 +0000

    today's commit

--- Gallery/models.py
+++ Gallery/models.py
@@ -17,5 +17,25 @@ class Image(models.Model):
     def __str__(self):
         return self.title
     
+class Album(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name="albums"
+        )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank = True)
+    images = models.ManyToManyField("images", related_name="albums", blank = True
+                                    )
+
+is_public = models.BooleanField(default=False)
+
+created_at = models.DateTimeField(autoS_now_add = True)
+
+def __str__(self):
+    return f"{self.title} ({self.owner})"
+
+    
 "https://dtiqleetduadxhqemuby.supabase.co"
 "sb_secret_LucIh5qSil6SUZqFiMLXmQ_6kr0sXi0"
